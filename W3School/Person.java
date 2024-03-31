eaf7688a6fcc7fe15b784c05a340c773527cc66b
muralishan96 <muralishantvkoil@gmail.com>
2024-03-31 21:34:13 +0530

    encapsulation

---
+++ W3School/Person.java
@@ -0,0 +1,14 @@
+package W3School;
+public class Person {
+    private String name;
+
+    //getter
+    public String getName(){
+        return name;
+    }
+
+    //Setter
+    public void setName(String sName){
+        this.name = sName;
+    }
+}
---
+++ W3School/Person1.java
@@ -0,0 +1,9 @@
+package W3School;
+
+public class Person1 {
+    public static void main(String[] args) {
+        Person obj = new Person();
+        obj.setName("Murali");
+        System.out.println(obj.getName());
+    }
+}
