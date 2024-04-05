777692ba9938c4bbbaea00d46f76f6fb406fb66b
muralishan96 <muralishantvkoil@gmail.com>
2024-04-05 16:44:55 +0530

    temp

---
+++ Oreilly/temp.java
@@ -0,0 +1,16 @@
+package Oreilly;
+
+public class temp {
+    
+    public static void main(String[] args) {
+        
+        int number = 42;
+        double decimal = 3.14;
+        boolean flag = true;
+
+        // Treat variables as strings when printing
+        System.out.println("The value of number is: " + number);
+        System.out.println("The value of decimal is: " + decimal);
+        System.out.println("The value of flag is: " + flag);
+    }
+}
